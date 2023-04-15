805d9e0b06d64b5d5ce754c3890495f306315792
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2023-04-15 14:27:19 +0200

    developer algorithms - Elementary Sorts -- Insertion Sort

--- 4-Elementary-Sorts/InsertionSort.go
+++ 4-Elementary-Sorts/InsertionSort.go
@@ -1,14 +1,15 @@
 package ElementarySorts
 
-// SelectionSort N^2
-func SelectionSort[ordered Ordered](list []ordered, callback OrderedCallback[ordered]) {
+// InsertionSort ~1/4 N^2
+func InsertionSort[ordered Ordered](list []ordered, callback OrderedCallback[ordered]) {
 	lenList := len(list)
 
 	for i := 0; i < lenList; i++ {
-		for j := i + 1; j < lenList; j++ {
-
+		for j := 0; j <= i; j++ {
 			if callback(list[i], list[j]) {
 				list[i], list[j] = list[j], list[i]
+			} else {
+				break
 			}
 		}
 	}
---
+++ 4-Elementary-Sorts/InsertionSort_test.go
@@ -0,0 +1,38 @@
+package ElementarySorts
+
+import "testing"
+
+func TestInsertionSort(t *testing.T) {
+	type args[ordered Ordered] struct {
+		list     []ordered
+		callback OrderedCallback[ordered]
+	}
+	type testCase[ordered Ordered] struct {
+		name string
+		args args[ordered]
+		want []ordered
+	}
+	tests := []testCase[int]{
+		{
+			name: "Case 1",
+			args: args[int]{
+				list: []int{1, 34, 3, 23, 6, 9, 22, 1, 5, 6, 9, 77, 5, 4, 3},
+				callback: func(a, b int) bool {
+					return a < b
+				},
+			},
+			want: []int{1, 1, 3, 3, 4, 5, 5, 6, 6, 9, 9, 22, 23, 34, 77},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := tt.args.list
+			InsertionSort[int](list, tt.args.callback)
+			for i, w := range tt.want {
+				if got := list[i]; got != w {
+					t.Errorf("SelectionSort() = %v, want %v", got, w)
+				}
+			}
+		})
+	}
+}
